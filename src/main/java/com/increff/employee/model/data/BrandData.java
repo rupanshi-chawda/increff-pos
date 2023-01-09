62c5ccb4e31e357bc45d5c57f8589c88d7858ffb
Rupanshi Chawda <rc80614@gmail.com>
2023-01-09 15:28:50 +0530

    added dto layer

---
+++ src/main/java/com/increff/employee/model/data/BrandData.java
@@ -0,0 +1,10 @@
+package com.increff.employee.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BrandData extends BrandForm{
+    private int id;
+}
---
+++ src/main/java/com/increff/employee/model/form/BrandForm.java
@@ -0,0 +1,13 @@
+package com.increff.employee.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BrandForm {
+
+    private String brand;
+    private String category;
+
+}
