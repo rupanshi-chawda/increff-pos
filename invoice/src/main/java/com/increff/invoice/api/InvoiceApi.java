5e675242bef126339e7b4e6fb2e9cfbde7fc5a2a
Rupanshi Chawda <rc80614@gmail.com>
2023-02-15 10:06:04 +0530

    refactored invoice app

--- invoice/src/main/java/com/increff/invoice/api/InvoiceApi.java
+++ invoice/src/main/java/com/increff/invoice/api/InvoiceApi.java
@@ -14,25 +14,12 @@ import java.util.Base64;
 @Service
 public class InvoiceApi {
     public String generateInvoice(InvoiceForm form) throws ApiException {
-        String _filename = "./Pdf/invoice_" + form.getOrderId() + ".pdf";
-        File f = new File(_filename);
-        if (!f.exists()) {
+
             XmlGeneration xmlGeneration = new XmlGeneration();
             xmlGeneration.createXML(form);
             String base64 = xmlGeneration.createXML(form);
             PdfFromFop pdfFromFOP = new PdfFromFop();
 
-            return pdfFromFOP.createPDF(form, base64);
-        } else {
-            Path pdfPath = Paths.get("./Pdf/invoice_" + form.getOrderId() + ".pdf");
-            byte[] pdf;
-            try {
-                pdf = Files.readAllBytes(pdfPath);
-            } catch (IOException e) {
-                throw new ApiException(e.getMessage());
-            }
-            String base64 = Base64.getEncoder().encodeToString(pdf);
-            return base64;
-        }
+            return pdfFromFOP.createPDF(base64);
     }
 }
--- invoice/src/main/java/com/increff/invoice/api/PdfFromFop.java
+++ invoice/src/main/java/com/increff/invoice/api/PdfFromFop.java
@@ -1,78 +1,49 @@
 package com.increff.invoice.api;
 
-import com.increff.invoice.model.InvoiceForm;
 import com.increff.invoice.util.ApiException;
 import org.apache.fop.apps.*;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.xml.transform.*;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Base64;
 
 public class PdfFromFop {
+    public String createPDF(String xmlEncodedString) throws ApiException {
 
-    @Value("${xsl.path}")
-    private String xsl_path;
+            String path = new File("src/main/resources/xsl/invoice.xsl").getAbsolutePath();
+            File xsltfile = new File(path);
 
-    public String createPDF(InvoiceForm form, String xmlEncodedString) throws ApiException {
+            // configure fopFactory as desired
+            final FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
 
-        File xsltfile = new File("C:\\Users\\KIIT\\Downloads\\increff-pos\\invoice\\src\\main\\resources\\xsl\\invoice.xsl");
-        File pdfDir = new File("./Pdf");
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        pdfDir.mkdirs();
-        File pdfFile = new File(pdfDir, "invoice_" + form.getOrderId() + ".pdf");
-        // configure fopFactory as desired
-        final FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
-        FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-
-        // Setup output
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        OutputStream outfile = null;
-
-        try {
-            // Construct fop with desired output format
-            outfile = new FileOutputStream(pdfFile);
-            outfile = new java.io.BufferedOutputStream(outfile);
-
-            Fop fop;
-            fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
-
-            // Setup XSLT
-            TransformerFactory factory = TransformerFactory.newInstance();
-            Transformer transformer = factory.newTransformer(new StreamSource(xsltfile));
-
-            // Setup input for XSLT transformation
-            Source src = convert(xmlEncodedString);
-            // Resulting SAX events (the generated FO) must be piped through to FOP
-            Result res = new SAXResult(fop.getDefaultHandler());
-            // Start XSLT transformation and FOP processing
-            transformer.transform(src, res);
-
-            fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, outfile);
-            res = new SAXResult(fop.getDefaultHandler());
-            transformer.transform(src, res);
-
-        } catch (FOPException | TransformerException e) {
-            throw new ApiException("FOP transformation not working" + e.getMessage());
-        } finally {
-            byte[] pdf = out.toByteArray();
-            String base64 = Base64.getEncoder().encodeToString(pdf);
-            Path path = Paths.get(pdfFile.getAbsolutePath());
             try {
-                Files.write(path, pdf);
-                outfile.close();
-            } catch (IOException e) {
-                throw new ApiException("File cannot be created" + e.getMessage());
-            }
-            return base64;
-        }
+                // Construct fop with desired output format
+                Fop fop;
+                fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+                // Setup XSLT
+                TransformerFactory factory = TransformerFactory.newInstance();
+                Transformer transformer = factory.newTransformer(new StreamSource(xsltfile));
+                // Setup input for XSLT transformation
+                Source src = convert(xmlEncodedString);
+                // Resulting SAX events (the generated FO) must be piped through to FOP
+                Result res = new SAXResult(fop.getDefaultHandler());
+                // Start XSLT transformation and FOP processing
+                transformer.transform(src, res);
 
+                byte[] pdf = out.toByteArray();
+                String base64 = Base64.getEncoder().encodeToString(pdf);
+                return base64;
+
+            } catch (FOPException | TransformerException e) {
+                throw new ApiException("Cannot Transform to create Invoice"+e.getMessage());
+            }
     }
+
 
     private static StreamSource convert(String base64EncodedString) {
         byte[] decodedBytes = Base64.getDecoder().decode(base64EncodedString);
